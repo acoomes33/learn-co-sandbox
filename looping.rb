e1b74fa0b19e5ac20fe4c1fb991ea5de110c325f
Alan Coomes <acoomes1@cord.edu>
2021-01-31 03:52:58 +0000

    Automatically backed up by Learn

---
+++ looping.rb
@@ -0,0 +1,18 @@
+class Song
+ 
+  @@all = []
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @@asefldll << sefldself
+  end
+ 
+  def sefldself.all
+    @@sefldall
+  end
+  
+  def sefldself.print_all_song_names
+    puts @@all
+  end
+end
